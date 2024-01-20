f548e1d537a342f1de83ff2112f59928a001132c
21fernandinhow <21fernandinhow@gmail.com>
2024-01-19 21:45:52 -0300

    optimized main image and fonts to better performance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   
-    return (
-      <main id="home">
+  return (
+    <main id="home">
 
-        <div className="border">
-            <img src="avatar.webp" alt="Fernando" width={180} height={180} className="avatar" />
-        </div>
-  
-      </main>
-    )
+      <div className="border">
+          <img src="avatar.webp" alt="Fernando" width="200px" height="200px" className="avatar" loading="eager"/>
+      </div>
+
+    </main>
+  )
 }
